f520dc93740a9393a21c747c9b0405df9593731c
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-20 20:48:36 +0530

    [Done Problem 1] SearchingAlgorithms/LinearSearch/Problem1.cpp

---
+++ Searching Algorithms/Linear Search/Problem1.cpp
@@ -0,0 +1,36 @@
+/*
+    Last Occurrence
+*/
+
+#include<iostream>
+using namespace std;
+
+int linearSearch(int array[], int n, int x);
+
+int main()
+{
+    int n, x, array[100000];
+    cin>>n>>x;
+
+    for(int i = 1; i <= n; i++)
+    {
+        cin>>array[i];
+    }
+
+    int res = linearSearch(array, n, x);
+    cout<<res;
+
+    return 0;
+}
+
+int linearSearch(int array[], int n, int x)
+{
+    for(int i = n; i >= 1; i--)
+    {
+        if(array[i] == x)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
