0354489bf04375a9cb01dd843fea81933911069e
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-21 00:05:35 +0530

    [Minor Change] SearchingAlgorthms/LinearSearch/Problem4.cpp

--- Searching Algorithms/Linear Search/Problem4.cpp
+++ Searching Algorithms/Linear Search/Problem4.cpp
@@ -12,21 +12,18 @@ class Solution
         int print2Largest(int array[], int n)
         {
             int max1 = 0, max2 = -1;
-            int max1_index = -1;
 
             for(int i = 0; i < n; i++)
             {
                 if(array[i] > max1)
                 {
                     max1 = array[i];
-                    max1_index = i;
                 }
             }
 
-            array[max1_index] = -1;
             for(int i = 0; i < n; i++)
             {
-                if(array[i] > max2)
+                if(array[i] > max2 && array[i] != max1)
                 {
                     max2 = array[i];
                 }
