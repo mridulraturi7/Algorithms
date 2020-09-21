829a25f78cf813aa86de55f1f668ae0b9d2f7670
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-21 21:28:33 +0530

    [Done Bubble Sort(OPTIMIZED) Implementation] SortingAlgorithms/BubbleSort/OptimizedImplementation.cpp

---
+++ Sorting Algorithms/Bubble Sort/OptimizedImplementation.cpp
@@ -0,0 +1,57 @@
+/*
+    Bubble Sort (Optimized Implementation).
+*/
+
+#include<iostream>
+
+using namespace std;
+
+void bubbleSort(int array[], int n)
+{
+    bool swapped;
+    for(int i = 0; i < n - 1; i++)
+    {
+        swapped = false;
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(array[j] > array[j + 1])
+            {
+                int temp;
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+
+                swapped = true;
+            }
+        }
+
+        if(swapped == false)
+        {
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of Input Array: ";
+    cin>>n;
+
+    cout<<"Enter Array Elements: ";
+    int *array = new int[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin>>array[i];
+    }
+
+    bubbleSort(array, n);
+
+    cout<<"Array after performing Bubble Sort: ";
+    for(int i = 0; i < n; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+
+    return 0;
+}
