578d949b2a50e162b1bd127b97ec3be24aec0072
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-21 21:13:16 +0530

    [Done Bubble Sort Implementation] SortingAlgorithms/Implementation.cpp

---
+++ Sorting Algorithms/Bubble Sort/Implementation.cpp
@@ -0,0 +1,46 @@
+/*
+    Bubble Sort.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+void bubbleSort(int array[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(array[j] > array[j + 1])
+            {
+                int temp;
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of Input Array: ";
+    cin>>n;
+
+    cout<<"Enter Array Elements: ";
+    int *array = new int[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin>>array[i];
+    }
+
+    bubbleSort(array, n);
+
+    cout<<"Array after performing bubble sort: ";
+    for(int i = 0; i < n; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+}
