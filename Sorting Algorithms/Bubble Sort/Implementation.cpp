403d26d94bf22944af227367f894d733a572e5ed
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-22 23:41:01 +0530

    [Modify] SortingAlgorithm/BubbleSort/Implementation.cpp

--- Sorting Algorithms/Bubble Sort/Implementation.cpp
+++ Sorting Algorithms/Bubble Sort/Implementation.cpp
@@ -11,9 +11,6 @@
     Space Complexity Analysis:
         Auxiliary Space = O(1)
 
-    Bubble Sort is In-Place Sorting Algorithm.
-    Bubble Sort is Stable Sorting Algorithm.
-
     --The following code will always run O(n^2) times even if the array is sorted.
     --We can optimize it by stopping the algorithm is the inner loop doesn't cause any swap operation.
 */
