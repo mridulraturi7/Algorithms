d5cd31154e5e754db17c2ca102ece1964274ea83
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-23 23:45:19 +0530

    [Done Insertion Sort - Recursive Implementation] SortingAlgorithm/InsertionSort/RecursiveImplementation.cpp

---
+++ Sorting Algorithms/Insertion Sort/RecursiveImplementation.cpp
@@ -0,0 +1,52 @@
+/*
+    Insertion Sort (Recursive Implementation)
+*/
+
+#include<iostream>
+
+using namespace std;
+
+void insertionSort(int array[], int n)
+{
+    if(n <= 1)
+        return;
+
+    insertionSort(array, n - 1);
+
+    int last = array[n - 1];
+    int j = n - 2;
+
+    while(j >= 0 && array[j] > last)
+    {
+        array[j + 1] = array[j];
+        j--;
+    }
+
+    array[j + 1] = last;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of Input Array: ";
+    cin>>n;
+
+    int *array = new int[n];
+    cout<<"Enter Array Elements: ";
+    for(int i = 0; i < n; i++)
+    {
+        cin>>array[i];
+    }
+
+    cout<<"Performing Insertion Sort......"<<endl;
+
+    insertionSort(array, n);
+
+    cout<<"Array after performing Insertion Sort: ";
+    for(int i = 0; i < n; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+
+    return 0;
+}
