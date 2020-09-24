da107cba14cb7f0b8b9722297efa149a60502f24
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-24 12:09:42 +0530

    [DONE Selection Sort Implementation] SortingAlgorithms/SelectionSort/Implementation.cpp

---
+++ Sorting Algorithms/Selection Sort/Implementation.cpp
@@ -0,0 +1,52 @@
+/*
+    Selection Sort.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+void selectionSort(int array[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        int min_index = i;
+        for(int j = i + 1; j < n; j++)
+        {
+            if(array[j] < array[min_index])
+            {
+                min_index = j;
+            }
+        }
+
+        int temp = array[min_index];
+        array[min_index] = array[i];
+        array[i] = temp;
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of Array: ";
+    cin>>n;
+
+    int *array = new int[n];
+    cout<<"Enter Array Elements: ";
+    for(int i = 0; i < n; i++)
+    {
+        cin>>array[i];
+    }
+
+    cout<<"Performing Selection Sort......"<<endl;
+
+    selectionSort(array, n);
+
+    cout<<"Array after performing Selection Sort: ";
+    for(int i = 0; i < n; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+
+    return 0;
+}
