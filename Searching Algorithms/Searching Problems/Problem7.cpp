1a68b68cb9efa91d75926b957eb50b736bfac91c
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-27 23:46:51 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem7.cpp

--- Searching Algorithms/Searching Problems/Problem7.cpp
+++ Searching Algorithms/Searching Problems/Problem7.cpp
@@ -1,5 +1,8 @@
 /*
     Find the element that appears once in sorted array.
+    This problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/find-the-element-that-appears-once-in-sorted-array0624/1/?category[]=Searching&problemStatus=unsolved&difficulty[]=-1&page=1&query=category[]SearchingproblemStatusunsolveddifficulty[]-1page1
+    Difficulty - Basic
 */
 
 #include<iostream>
