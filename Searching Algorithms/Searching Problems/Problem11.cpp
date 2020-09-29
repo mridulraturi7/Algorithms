8d7929655a94a9903e0f71e208b2feb3b2705746
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-29 21:11:43 +0530

    [Done Problem 11] SeachingAlgorithms/SearchingProblems/Problem11.cpp

---
+++ Searching Algorithms/Searching Problems/Problem11.cpp
@@ -0,0 +1,39 @@
+/*
+    Search an element in an Array.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, k;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        cin>>k;
+
+        int index = -1;
+        for(int i = 0; i < n; i++)
+        {
+            if(array[i] == k)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        cout<<index<<endl;
+    }
+}
