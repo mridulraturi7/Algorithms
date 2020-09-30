a05c0b9284e94d9fb9491d17cf1ef2255840bb54
mridulraturi7 <mridulraturi7@gmail.com>
2020-09-30 20:47:24 +0530

    [WIP Problem 14] SearchingAlgorithms/SearchingProblems/Problem14.cpp

---
+++ Searching Algorithms/Searching Problems/Problem14.cpp
@@ -0,0 +1,44 @@
+/*
+    Missing Number in an Array.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+class Solution
+{
+    public:
+        int missingNumber(int array[], int n)
+        {
+            int total_sum, current_sum = 0;
+            total_sum = (n * (n + 1)) / 2;
+            for(int i = 0; i < n; i++)
+            {
+                current_sum += array[i];
+            }
+
+            return total_sum - current_sum;
+        }
+};
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        Solution s;
+        cout<<s.missingNumber(array, n)<<endl;
+    }
+}
