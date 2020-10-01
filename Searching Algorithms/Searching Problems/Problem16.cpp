2fbc85b509af1fe55103b37c073485cd7043d4b7
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-01 20:40:35 +0530

    [WIP Problem 16] SearchingAlgorithms/SearchingProblems/Problem16.cpp

---
+++ Searching Algorithms/Searching Problems/Problem16.cpp
@@ -0,0 +1,59 @@
+/*
+    Count the Zeros.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+class Solution
+{
+    public:
+        int countZeroes(int array[], int n)
+        {
+            int left = 0, right = n - 1;
+            while(left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if((left == right) && (array[mid] == 0))
+                {
+                    return (n - mid + 1);
+                }
+
+                else if(array[mid] == 1)
+                {
+                    left = mid + 1;
+                }
+
+                else
+                {
+                    right = mid - 1;
+                }   
+            }
+            return 0;
+        }
+};
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        Solution s;
+        cout<<s.countZeroes(array, n)<<endl;
+    }
+
+    return 0;
+}
