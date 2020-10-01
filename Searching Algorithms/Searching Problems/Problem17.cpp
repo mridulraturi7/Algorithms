8f613142450dd8b8454198a9baeb46330c4f19c1
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-01 23:44:14 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem17.cpp

--- Searching Algorithms/Searching Problems/Problem17.cpp
+++ Searching Algorithms/Searching Problems/Problem17.cpp
@@ -1,5 +1,8 @@
 /*
     Maximum Value in a Bitonic Array.
+    This problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/maximum-value-in-a-bitonic-array3001/1/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
+    Difficulty - Easy
 */
 
 #include<iostream>
