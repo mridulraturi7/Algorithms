ae87843461a33be7499866a89b8f1dd4bd82348f
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-02 11:41:06 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem18.cpp

--- Searching Algorithms/Searching Problems/Problem18.cpp
+++ Searching Algorithms/Searching Problems/Problem18.cpp
@@ -1,5 +1,8 @@
 /*
     Find Transition Point.
+    This problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/find-transition-point-1587115620/1/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
+    Difficulty - Easy
 */
 
 #include<iostream>
