79ad37ff80659eda7229da8af2b5c6c8cd1c4493
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-02 13:01:41 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem20.cpp

--- Searching Algorithms/Searching Problems/Problem20.cpp
+++ Searching Algorithms/Searching Problems/Problem20.cpp
@@ -1,5 +1,8 @@
 /*
     Peak Element.
+    This problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/peak-element/1/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
+    Difficulty - Easy
 */
 
 #include<iostream>
