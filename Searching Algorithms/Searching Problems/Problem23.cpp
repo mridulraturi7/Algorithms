52996d82050b9107dcb8d5dd858b185cb630f1e3
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-03 13:47:53 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem23.cpp

--- Searching Algorithms/Searching Problems/Problem23.cpp
+++ Searching Algorithms/Searching Problems/Problem23.cpp
@@ -1,5 +1,8 @@
 /*
     Counting elements in two arrays.
+    This problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/counting-elements-in-two-arrays/1/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
+    Difficulty - Easy
 */
 
 #include<iostream>
