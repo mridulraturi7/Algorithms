7ffdf3e0e0d458c45ebc3247bd072d6e477dcd49
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-03 13:42:51 +0530

    [WIP Problem 23] SearchingAlgorithms/SearchingProblems/Problem23.cpp

---
+++ Searching Algorithms/Searching Problems/Problem23.cpp
@@ -0,0 +1,71 @@
+/*
+    Counting elements in two arrays.
+*/
+
+#include<iostream>
+#include<vector>
+#include<numeric>
+#include<algorithm>
+
+using namespace std;
+
+vector<int> countEleLessThanOrEqual(int array1[], int array2[], int m, int n)
+{
+    int max = *max_element(array2, array2 + n);
+    int *hash = new int[max + 1] ();
+    for(int i = 0; i < n; i++)
+    {
+        hash[array2[i]]++;
+    }
+
+    vector<int> res(m);
+    for(int i = 0; i < m; i++)
+    {
+        int sum = 0;
+        if(array1[i] > max)
+        {
+            res[i] = m;
+        }
+        else
+        {
+            res[i] = accumulate(hash, hash + array1[i] + 1, sum);   
+        }
+    }
+
+    return res;
+}
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int m, n;
+        cin>>m>>n;
+
+        int *array1 = new int[m];
+        for(int i = 0; i < m; i++)
+        {
+            cin>>array1[i];
+        }
+
+        int *array2 = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array2[i];
+        }
+
+        vector<int> res = countEleLessThanOrEqual(array1, array2, m, n);
+
+        for(int i = 0; i < res.size(); i++)
+        {
+            cout<<res[i]<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
