986283968839bc792e30435c4de73546dfabe503
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-03 11:57:41 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem21.cpp

--- Searching Algorithms/Searching Problems/Problem21.cpp
+++ Searching Algorithms/Searching Problems/Problem21.cpp
@@ -1,5 +1,8 @@
 /*
     Index of an Extra Element.
+    This problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/index-of-an-extra-element/1/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
+    Difficulty - Easy
 */
 
 #include<iostream>
