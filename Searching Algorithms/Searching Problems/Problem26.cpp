b04072337ebad3b86a43d9c6082ad11fa933931d
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-04 18:29:08 +0530

    [WIP Problem 26 - Approach 1] SearchingAlgorithms/SearchingProblems/Problem26.cpp

---
+++ Searching Algorithms/Searching Problems/Problem26.cpp
@@ -0,0 +1,63 @@
+/*
+    Array subset of another array.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int check(int array1[], int m, int key)
+{
+    for(int i = 0; i < m; i++)
+    {
+        if(array1[i] == key)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int m, n;
+        cin>>m>>n;
+
+        int *array1 = new int[m];
+        for(int i = 0; i < m; i++)
+        {
+            cin>>array1[i];
+        }
+
+        int *array2 = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array2[i];
+        }
+
+        int common = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            int checkIfPresent = check(array1, m, array2[i]);
+            common += checkIfPresent;
+        }
+
+        if(common == m)
+        {
+            cout<<"Yes"<<endl;
+        }
+
+        else
+        {
+            cout<<"No"<<endl;
+        }
+        
+    }
+}
