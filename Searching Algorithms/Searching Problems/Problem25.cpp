45dd3ef8c10330bfb92c11de57b3581f2ef6d109
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-04 18:08:59 +0530

    [Done Problem 25] SearchingAlgorithms/SearchingProblems/Problem25.cpp

---
+++ Searching Algorithms/Searching Problems/Problem25.cpp
@@ -0,0 +1,44 @@
+/*
+    Facing the Sun.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int countBuildings(int array[], int n)
+{
+    int left_most = array[0], count = 0;
+    for(int i = 1; i < n; i++)
+    {
+        if(array[i] >= left_most)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int buildings = countBuildings(array, n);
+        cout<<buildings<<endl;
+    }
+
+    return 0;
+}
