466bacb9778c98d5e7967508588da83f248db0dc
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-05 23:25:54 +0530

    [WIP Problem 28] SearchingAlgorithms/SearchingProblems/Problem28.cpp

---
+++ Searching Algorithms/Searching Problems/Problem28.cpp
@@ -0,0 +1,54 @@
+/*
+    Smallest Greater Elements in whole array.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            int min_diff = 1000000, nextGreater = -1;
+            for(int j = 0; j < n; j++)
+            {
+                if(array[j] > array[i])
+                {
+                    int current_diff = array[j] - array[i];
+                    if(current_diff < min_diff)
+                    {
+                        min_diff = current_diff;
+                        nextGreater = array[j];
+                    }
+                }
+            }
+
+            if(nextGreater == -1)
+            {
+                cout<<"-1"<<" ";
+            }
+
+            else
+            {
+                cout<<nextGreater<<" ";
+            }
+        }
+
+        cout<<endl;
+    }
+}
