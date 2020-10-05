ebc82db7b370bceff815e0a02a8262711c5da2e6
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-05 20:47:54 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem26.cpp

--- Searching Algorithms/Searching Problems/Problem26.cpp
+++ Searching Algorithms/Searching Problems/Problem26.cpp
@@ -1,5 +1,5 @@
 /*
-    Array subset of another array.
+    Array subset of another array - Approach 1 - Brute Force.
     This problem is taken from GFG.
     Problem Statement - https://practice.geeksforgeeks.org/problems/array-subset-of-another-array/0/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
     Difficulty - Easy
