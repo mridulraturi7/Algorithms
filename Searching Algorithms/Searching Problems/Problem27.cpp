bf70f901b70591dc0dfea0b1ed54b02b61a44eb3
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-05 21:01:20 +0530

    [WIP Problem 27 - Approach 2] SearchingAlgorithms/SearchingProblems/Problem27.cpp

---
+++ Searching Algorithms/Searching Problems/Problem27.cpp
@@ -0,0 +1,55 @@
+/*
+    Array subset of another array - Approach 1 - Hashing.
+*/
+
+#include<iostream>
+#include<unordered_map>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int m, n;
+        cin>>m>>n;
+
+        int *array1 = new int[m];
+        for(int i = 0; i < m; i++)
+        {
+            cin>>array1[i];
+        }
+
+        int *array2 = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array2[i];
+        }
+
+        unordered_map<int, int> hash;
+        for(int i = 0; i < m; i++)
+        {
+            hash[array1[i]] = 1;
+        }
+
+        int allPresent = 1;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(hash.find(array2[i]) == hash.end())
+            {
+                allPresent = 0;
+                cout<<"No"<<endl;
+            }
+        }
+
+        if(allPresent == 1)
+        {
+            cout<<"Yes"<<endl;
+        }
+    }
+    return 0;
+}
