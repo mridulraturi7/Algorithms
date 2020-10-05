de0c4114fa5739d002be81a875becd7ce6f1e194
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-05 21:03:48 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem27.cpp

--- Searching Algorithms/Searching Problems/Problem27.cpp
+++ Searching Algorithms/Searching Problems/Problem27.cpp
@@ -1,5 +1,8 @@
 /*
     Array subset of another array - Approach 1 - Hashing.
+    This problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/array-subset-of-another-array/0/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
+    Difficulty - Easy 
 */
 
 #include<iostream>
