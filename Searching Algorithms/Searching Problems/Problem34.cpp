f55c8d69484e90eb1f068e484a6395e61e830b72
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-08 13:46:09 +0530

    [Minor Update] SearchingAlgorithms/SearchingProblems/Problem34.cpp

--- Searching Algorithms/Searching Problems/Problem34.cpp
+++ Searching Algorithms/Searching Problems/Problem34.cpp
@@ -2,40 +2,48 @@
     Search in a Rotated Array (Approach 1 - Using Binary Search).
 */
 
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int findPivotPoint(int array[], int n)
+long findPivotPoint(long long array[], long n)
 {
-    int l = 0, r = n - 1;
+    long l = 0, r = n - 1;
     while(l <= r)
     {
-        int mid = (l + r) / 2;
-
-        if(array[mid + 1] < array[mid])
+        long mid = (l + r) / 2;
+        
+        if(l == r) 
+        return l;
+        
+        else if(mid < r && array[mid] > array[mid + 1]) 
         {
             return mid;
         }
-
-        else if(array[l] <= array[mid])
+  
+        else if(mid > l && array[mid] < array[mid - 1]) 
         {
-            l = mid + 1;
-        }
-
-        else
+            return (mid - 1);
+        } 
+  
+        else if(array[l] >= array[mid]) 
         {
             r = mid - 1;
+        }
+  
+        else
+        {
+            l = mid + 1;
         }
     }
     return -1;
 }
 
-int binarySearch(int array[], int l, int r, int k)
+long binarySearch(long long array[], long l, long r, long long k)
 {
     while(l <= r)
     {
-        int mid = (l + r) / 2;
+        long mid = (l + r) / 2;
         if(array[mid] == k)
         {
             return mid;
@@ -62,21 +70,21 @@ int main()
 
     while(testCase-- != 0)
     {
-        int n, k;
+        long n;
+        long long k;
         cin>>n;
 
-        int *array = new int[n];
-        for(int i = 0; i < n; i++)
+        long long *array = new long long[n];
+        for(long i = 0; i < n; i++)
         {
             cin>>array[i];
         }
 
         cin>>k;
 
-        int pivot = findPivotPoint(array, n);
+        long pivot = findPivotPoint(array, n);
 
-    
-        int found;
+        long found;
         
         if(k <= array[pivot] && k >= array[0])
         {
