900e28fdd69fb7b0441cf55c662c8732a018928a
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-09 23:19:50 +0530

    [Done Problem 36 - Approach 1] SearchingAlgorithms/SearchingProblems/Problem36.cpp

--- Searching Algorithms/Searching Problems/Problem36.cpp
+++ Searching Algorithms/Searching Problems/Problem36.cpp
@@ -12,7 +12,18 @@ class Solution
     public:
         int matSearch(vector<vector<int>> &mat, int N, int M, int X)
         {
+            for(int i = 0; i < N; i++)
+            {
+                for(int j = 0; j < M; j++)
+                {
+                    if(mat[i][j] == X)
+                    {
+                        return 1;
+                    }
+                }
+            }
 
+            return 0;
         }
 };
 
