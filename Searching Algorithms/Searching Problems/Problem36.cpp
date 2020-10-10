5eb449a390a35ff7cf0c64fb733dc940566f9d52
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-10 23:29:32 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem36.cpp

--- Searching Algorithms/Searching Problems/Problem36.cpp
+++ Searching Algorithms/Searching Problems/Problem36.cpp
@@ -1,6 +1,5 @@
 /*
     Search in a Matrix - Approach - 1 - Linear Search.
-    This problem is taken from GFG.
     Problem Statement - https://practice.geeksforgeeks.org/problems/search-in-a-matrix17201720/1/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
     Difficulty - Easy
 
