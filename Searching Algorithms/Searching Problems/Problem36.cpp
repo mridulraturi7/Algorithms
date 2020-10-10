26ec67829e0c172a413254556830aca5625fdcb5
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-10 23:29:48 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem36.cpp

--- Searching Algorithms/Searching Problems/Problem36.cpp
+++ Searching Algorithms/Searching Problems/Problem36.cpp
@@ -3,9 +3,6 @@
     This problem is taken from GFG.
     Problem Statement - https://practice.geeksforgeeks.org/problems/search-in-a-matrix17201720/1/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
     Difficulty - Easy
-
-    Time Complexity - O(n*n)
-    Space Complexity - O(1)
 */
 
 #include<iostream>
