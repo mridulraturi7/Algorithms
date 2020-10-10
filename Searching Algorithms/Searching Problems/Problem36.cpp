bd3a17ae85e81c7ca8360a5236941de491713c40
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-10 23:29:59 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem36.cpp

--- Searching Algorithms/Searching Problems/Problem36.cpp
+++ Searching Algorithms/Searching Problems/Problem36.cpp
@@ -3,6 +3,9 @@
     This problem is taken from GFG.
     Problem Statement - https://practice.geeksforgeeks.org/problems/search-in-a-matrix17201720/1/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
     Difficulty - Easy
+
+    Time Complexity - O(n*n)
+    Space Complexity - O(1)
 */
 
 #include<iostream>
@@ -25,6 +28,7 @@ class Solution
                     }
                 }
             }
+            
             return 0;
         }
 };
