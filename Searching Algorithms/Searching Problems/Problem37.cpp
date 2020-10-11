7c19b2261abfea418432f7d5e73eed9a1811ecdf
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-11 13:50:34 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem37.cpp

--- Searching Algorithms/Searching Problems/Problem37.cpp
+++ Searching Algorithms/Searching Problems/Problem37.cpp
@@ -1,5 +1,5 @@
 /*
-    Search in a Matrix - Approach 2
+    Search in a Matrix - Approach 2 (Optimized).
     This problem is taken from GFG.
     Problem Statement - https://practice.geeksforgeeks.org/problems/search-in-a-matrix17201720/1/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
     Difficulty - Easy
