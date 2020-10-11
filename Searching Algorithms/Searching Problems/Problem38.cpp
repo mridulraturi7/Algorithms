d55be1e4cb8dca4d3d5834403f503fb114faf878
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-11 23:13:43 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem38.cpp

--- Searching Algorithms/Searching Problems/Problem38.cpp
+++ Searching Algorithms/Searching Problems/Problem38.cpp
@@ -1,5 +1,8 @@
 /*
     Number and the Digit Sum.
+    This problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/number-and-the-digit-sum4021/1/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
+    Difficulty - Easy
 */
 
 #include<iostream>
