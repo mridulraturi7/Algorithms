165f9e5d1dc156810544785dace7b31188ac3a4d
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-12 20:08:07 +0530

    [Done Problem 42] SearchingAlgorithms/SearchingProblems/Problem42.cpp

---
+++ Searching Algorithms/Searching Problems/Problem42.cpp
@@ -0,0 +1,55 @@
+/*
+    Find the closest pair from two arrays.
+*/
+
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, m;
+        cin>>n>>m;
+
+        int *A = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>A[i];
+        }
+
+        int *B = new int[m];
+        for(int i = 0; i < m; i++)
+        {
+            cin>>B[i];
+        }
+
+        int x;
+        cin>>x;
+
+        int min_difference = 500, minA, minB;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                int sum = A[i] + B[j];
+                int current_diff = abs(x - sum);
+                if(current_diff < min_difference)
+                {
+                    min_difference = current_diff;
+                    minA = A[i];
+                    minB = B[j];
+                }
+            }
+        }
+
+        cout<<minA<<" "<<minB<<endl;
+    }
+
+    return 0;
+}
