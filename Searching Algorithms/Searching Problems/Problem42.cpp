f0e81e4d3bd63b9471ecbfa96d4455c98972b97b
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-12 20:36:12 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem42.cpp

--- Searching Algorithms/Searching Problems/Problem42.cpp
+++ Searching Algorithms/Searching Problems/Problem42.cpp
@@ -1,5 +1,8 @@
 /*
     Find the closest pair from two arrays.
+    This problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/find-the-closest-pair-from-two-arrays/0/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
+    Difficulty - Easy
 */
 
 #include <iostream>
