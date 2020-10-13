59d6eccd3f77d0b9c00f48862e716341b6574537
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-13 12:38:18 +0530

    [Done Problem 43] SearchingAlgorithms/SearchingProblems/Problem43.cpp

---
+++ Searching Algorithms/Searching Problems/Problem43.cpp
@@ -0,0 +1,67 @@
+/*
+    Minimum number in a Sorted Rotated Array.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int findPivot(int array[], int n)
+{
+    int l = 0, r = n - 1;
+    while(l <= r)
+    {
+        int mid = (l + r) / 2;
+
+        if(l == r)
+        {
+            return l;
+        }
+
+        else if(mid < r && array[mid] > array[mid + 1])
+        {
+            return mid;
+        }
+
+        else if(mid > l && array[mid] < array[mid - 1])
+        {
+            return mid - 1;
+        }
+
+        else if(array[l] >= array[mid])
+        {
+            r = mid - 1;
+        }
+
+        else
+        {
+            l = mid + 1;
+        }
+    }
+
+    return n - 1;
+}
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        int *array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+
+        int pivot = findPivot(array, n);
+        (pivot != n - 1) ? cout<<array[pivot + 1] : cout<<array[0];
+        cout<<endl;
+    }
+
+    return 0;
+}
