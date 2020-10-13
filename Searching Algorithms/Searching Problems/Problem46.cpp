7719a13c2ab2adcfcf4366e4660618673738d381
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-14 01:56:06 +0530

    [Modify] SearchingALgorithms/SearchingProblems/Problem46.cpp

--- Searching Algorithms/Searching Problems/Problem46.cpp
+++ Searching Algorithms/Searching Problems/Problem46.cpp
@@ -1,5 +1,8 @@
 /*
     Two Repeated Elements.
+    This problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/two-repeated-elements/0/?category[]=Searching&difficulty[]=0&page=1&query=category[]Searchingdifficulty[]0page1
+    Difficulty - Easy
 */
 
 #include <iostream>
