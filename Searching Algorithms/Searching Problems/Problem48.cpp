7c3d3cd99742d9a80eb1f7b86bdc08f4a9d532c6
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-15 23:36:10 +0530

    [WIP Problem 48] SearchingAlgorithms/SearchingProblems/Problem48.cpp

--- Searching Algorithms/Searching Problems/Problem48.cpp
+++ Searching Algorithms/Searching Problems/Problem48.cpp
@@ -7,6 +7,54 @@
 
 using namespace std;
 
+class Solution
+{
+    public:
+        int maxOnes(vector<vector<int>> &mat, int n, int m)
+        {
+            int max_row_zero = 0, row = 0;
+            for(int i = 0; i < n; i++)
+            {
+                int l = mat[i][0], r = mat[i][m - 1];
+                int current_row_zero = 0;
+
+                if(mat[i][0] == 1)
+                {
+                    return i;
+                }
+
+                while(l <= r)
+                {
+                    int mid = (l + r) / 2;
+
+                    if(mat[i][mid] == 1 && mat[i][mid - 1] == 0)
+                    {
+                        current_row_zero = mat[i][mid];
+                        break;
+                    }
+                    
+                    else if(mat[i][mid - 1] == 1)
+                    {
+                        r = mid - 1;
+                    }
+
+                    else
+                    {
+                        l = mid + 1;
+                    }
+                }
+
+                if(current_row_zero > max_row_zero)
+                {
+                    max_row_zero = current_row_zero;
+                    row = i;
+                }
+            }
+
+            return row;
+        }
+};
+
 int main()
 {
     int testCase;
