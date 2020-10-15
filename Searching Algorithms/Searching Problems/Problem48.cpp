89a9ae92851870b04dfea848f75c8666afd71f47
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-15 23:26:59 +0530

    [Edit Problem 48] SearchingAlgorithms/SearchingProblems/Problem48.cpp

--- Searching Algorithms/Searching Problems/Problem48.cpp
+++ Searching Algorithms/Searching Problems/Problem48.cpp
@@ -2,3 +2,31 @@
     Maximum No. of 1's row.
 */
 
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n, m;
+        cin>>n>>m;
+
+        vector<vector<int>> arr(n, vector<int> (m));
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                cin>>arr[i][j];
+            }
+        }
+
+        Solution ob;
+        cout<<ob.maxOnes(arr, n, m)<<endl;
+    }
+}
