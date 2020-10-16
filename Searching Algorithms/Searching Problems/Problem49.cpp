385fc75600551284a32f32dff5f5c04b4a03983a
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-16 11:49:16 +0530

    [WIP Problem 49] SeachingAlgorithms/SearchingProblems/Problem49.cpp

---
+++ Searching Algorithms/Searching Problems/Problem49.cpp
@@ -0,0 +1,76 @@
+/*
+    Next Greater number with same set of digits.
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+void findNextGreater(vector<int> digits)
+{
+    int i, j;
+
+    //Find the element starting from right whose left element is smaller than element on its right.
+    for(i = digits.size() - 1; i >= 0; i--)
+    {
+        if(digits[i] > digits[i - 1])
+        {
+            break;
+        }
+    }
+
+    //Check if the number is in decreasing order, if i == 0
+    if(i == 0)
+    {
+        cout<<"Not Possible"<<endl;
+        return;
+    }
+
+    int x = digits[i - 1], smallest = i;
+    for(j = i + 1; j < digits.size(); j++)
+    {
+        if(digits[j] > x && digits[j] < digits[smallest])
+        {
+            smallest = j;
+        }
+    }
+
+    //swap both digits
+    int temp = digits[i];
+    digits[i] = digits[smallest];
+    digits[smallest] = temp;
+
+    sort(digits.begin() + i, digits.end());
+
+    for(int k = 0; k < digits.size(); i++)
+    {
+        cout<<digits[k];
+    }
+}
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int num;
+        cin>>num;
+
+        vector<int> digits;
+        while(num != 0)
+        {
+            digits.push_back(num % 10);
+            num /= 10;
+        }
+
+        reverse(digits.begin(), digits.end());
+
+        findNextGreater(digits);
+    }
+
+    return 0;
+}
