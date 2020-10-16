0242579aa2f6cc651cacbd8d1c16b5778dfe3108
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-16 23:28:02 +0530

    [Modify] SeachingAlgorithms/SearchingProblems/Problem50.cpp

--- Searching Algorithms/Searching Problems/Problem50.cpp
+++ Searching Algorithms/Searching Problems/Problem50.cpp
@@ -4,7 +4,7 @@
     Problem Statement - https://practice.geeksforgeeks.org/problems/subarray-with-given-sum/0/?category[]=Searching&problemStatus=unsolved&difficulty[]=0&page=1&query=category[]SearchingproblemStatusunsolveddifficulty[]0page1
     Difficulty - Easy
 
-    Time Complexity - O(n^3)
+    Time Complexity - O(n^2)
     Space Complexity - O(1)
 */
 
