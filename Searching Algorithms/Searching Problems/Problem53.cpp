f7c3694c92d75a6d07def8acb4c9eb5e871b4003
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-18 23:55:10 +0530

    [WIP Problem 53] SearchinAlgorithms/SearchingProblems/Problem53.cpp

--- Searching Algorithms/Searching Problems/Problem53.cpp
+++ Searching Algorithms/Searching Problems/Problem53.cpp
@@ -11,7 +11,31 @@ class Solution
     public:
         int findMissing(int array[], int n)
         {
+            int c_d = (array[n - 1] - array[0]) / n;
+            int l = 0, r = n - 1;
 
+            while(l <= r)
+            {
+                int mid = (l + r) / 2;
+                int expected_element = (array[0] + (mid * c_d));
+
+                if(array[mid] != expected_element && array[mid - 1] == (expected_element - c_d))
+                {
+                    return expected_element;
+                }
+
+                else if(array[mid] == expected_element)
+                {
+                    l = mid + 1;
+                }
+
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+
+            return -1;
         }
 };
 
