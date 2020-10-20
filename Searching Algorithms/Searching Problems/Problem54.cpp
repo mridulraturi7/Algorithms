4b4bc3c602551d5b9a1b51093f4d72b183277f60
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-20 20:38:33 +0530

    [Create] SeaarchingAlgorithms/SearchingProblems/Problem54.cpp

---
+++ Searching Algorithms/Searching Problems/Problem54.cpp
@@ -0,0 +1,33 @@
+/*
+    Smallest Factorial Number.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+class Solution
+{
+    public:
+        int findNum(int n)
+        {
+
+        }
+};
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+
+    while(testCase-- != 0)
+    {
+        int n;
+        cin>>n;
+
+        Solution ob;
+        cout<<ob.findNum(n)<<endl;
+    }
+
+    return 0;
+}
