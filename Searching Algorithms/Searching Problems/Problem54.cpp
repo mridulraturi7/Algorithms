cc1b89860f64e03f677ee4f4729d716fa3815adb
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-20 20:46:37 +0530

    [Modify] SeaarchingAlgorithms/SearchingProblems/Problem54.cpp

--- Searching Algorithms/Searching Problems/Problem54.cpp
+++ Searching Algorithms/Searching Problems/Problem54.cpp
@@ -1,5 +1,8 @@
 /*
     Smallest Factorial Number.
+    This problem is taken from GFG.
+    Problem Statement - https://practice.geeksforgeeks.org/problems/smallest-factorial-number5929/1/?category[]=Searching&problemStatus=unsolved&difficulty[]=0&page=1&query=category[]SearchingproblemStatusunsolveddifficulty[]0page1#
+    Difficulty - Easy
 */
 
 #include<iostream>
