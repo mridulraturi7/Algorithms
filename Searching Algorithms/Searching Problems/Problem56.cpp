62bf4409c5a7f986497038ed5c2f817061cc7666
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-21 23:33:13 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem56.cpp

--- Searching Algorithms/Searching Problems/Problem56.cpp
+++ Searching Algorithms/Searching Problems/Problem56.cpp
@@ -10,7 +10,10 @@ using namespace std;
 class Solution
 {
     public:
-        
+        vector<int> commonElements(int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+            
+        }
 };
 
 int main()
