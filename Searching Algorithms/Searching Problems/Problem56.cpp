ea2cf2002acca89c66283f29b73cd1f9a31928b0
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-22 11:33:01 +0530

    [WIP] SearchingAlgorithms/SearchingProblems/Problem56.cpp

--- Searching Algorithms/Searching Problems/Problem56.cpp
+++ Searching Algorithms/Searching Problems/Problem56.cpp
@@ -18,7 +18,44 @@ class Solution
     public:
         vector<int> commonElements(int A[], int B[], int C[], int n1, int n2, int n3)
         {
-            
+            int max1 = A[n1 - 1], max2 = B[n2 - 1], max3 = C[n3 - 1];
+
+            int max = (max1 > max2) ? (max1 > max3 ? max1 : max3) : (max2 > max3 ? max2 : max3);
+
+            int *hash = new int[max + 1] ();
+
+            for(int i = 0; i < n1; i++)
+            {
+                hash[A[i]] = 1;
+            }
+
+            for(int i = 0; i < n2; i++)
+            {
+                if(hash[B[i]] == 1)
+                {
+                    hash[B[i]] = 2;
+                }
+            }
+
+            for(int i = 0; i < n3; i++)
+            {
+                if(hash[C[i]] == 2)
+                {
+                    hash[C[i]] = 3;
+                }
+            }
+
+            vector<int> res;
+
+            for(int i = 0; i < n1; i++)
+            {
+                if(hash[A[i]] == 3)
+                {
+                    res.push_back(A[i]);
+                }
+            }
+
+            return res;
         }
 };
 
@@ -52,6 +89,20 @@ int main()
         }
 
         Solution ob;
+
+        auto res = ob.commonElements(A, B, C, n1, n2, n3);
+
+        if(res.size() == 0)
+        {
+            cout<<"-1";
+        }
+
+        for(int i = 0; i < res.size(); i++)
+        {
+            cout<<res[i]<<" ";
+        }
+
+        cout<<endl;
     }
 
     return 0;
