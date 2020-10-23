701fcbcbb3bde2cf35b5b16e534a04a0ba094c56
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-23 23:46:01 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem57.cpp

--- Searching Algorithms/Searching Problems/Problem57.cpp
+++ Searching Algorithms/Searching Problems/Problem57.cpp
@@ -4,8 +4,7 @@
     Problem Statement - https://practice.geeksforgeeks.org/problems/common-elements1132/1/?category[]=Searching&problemStatus=unsolved&difficulty[]=0&page=1&query=category[]SearchingproblemStatusunsolveddifficulty[]0page1
     Difficulty - Easy
 
-    Time Complexity - O(n1 + n2 + n3)
-    Space Complexity - O(n1 + n2 + n3)
+    
 */
 
 #include<iostream>
