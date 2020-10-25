ad77db3678eb12dc35039a84e03f2fa50c349683
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-25 23:48:30 +0530

    [Modify] SearchingAlgorithms/SearchingProblems/Problem59.cpp

--- Searching Algorithms/Searching Problems/Problem59.cpp
+++ Searching Algorithms/Searching Problems/Problem59.cpp
@@ -1,5 +1,8 @@
 /*
     Union of two sorted arrays.
+    This problem is taken from GFG.
+    Problem Statement - https://www.geeksforgeeks.org/union-and-intersection-of-two-sorted-arrays-2/
+    Difficulty - Easy
 */
 
 #include<iostream>
