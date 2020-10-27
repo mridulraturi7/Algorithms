cdd96ac0b530b4c3322e9295cdfa64beb0f0256e
mridulraturi7 <mridulraturi7@gmail.com>
2020-10-27 23:17:50 +0530

    [Modify] SortingAlgorithms/QuickSort/Implementation.cpp

--- Sorting Algorithms/Quick Sort/Implementation.cpp
+++ Sorting Algorithms/Quick Sort/Implementation.cpp
@@ -1,5 +1,11 @@
 /*
     Quick Sort.
+
+    Quick Sort is a Divide and Conquer Algorithm.
+
+    It picks an element as pivot and partitions the array around the picked pivot element. The pivot element is 
+    sorted such that allthe elements to its left side will be less than that pivot element and all the elements 
+    to the right of the pivot element will be greater than the pivot element.
 */
 
 #include<iostream>
