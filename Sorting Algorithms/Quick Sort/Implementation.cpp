1db5dc6b50218d6bc0c1cb1ac18c344fbe459eb2
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-06 23:42:16 +0530

    [Modify] SortingAlgorithms/QuickSort/Implementation.cpp

--- Sorting Algorithms/Quick Sort/Implementation.cpp
+++ Sorting Algorithms/Quick Sort/Implementation.cpp
@@ -13,8 +13,7 @@
         Worst Case      : O(n*n)
 
     Space Complexity Analysis:
-        Since Quick Sort is a recursive algorithm and it does not use any extra space but it will use a stack 
-        to store the recursive function call.
+        
             Stack Size in Best Case  : O(logn)
             Stack Size in Worst Case : O(n)
 
