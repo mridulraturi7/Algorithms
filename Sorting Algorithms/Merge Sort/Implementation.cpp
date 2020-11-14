6889a840ad5292666ec70c299db58c88f37ca7f2
mridulraturi7 <mridulraturi7@gmail.com>
2020-11-14 23:32:15 +0530

    [UPDATE] SortingAlgorithms/MergeSort/Implementation.cpp

--- Sorting Algorithms/Merge Sort/Implementation.cpp
+++ Sorting Algorithms/Merge Sort/Implementation.cpp
@@ -13,7 +13,9 @@
     Space Complexity Analysis:
         Auxiliary Space : O(n)
 
-    
+    Merge Sort is not an "In Place" Sorting Algorithm.
+
+    Merge Sort is a Stable Sorting Algorithm.
 */
 
 #include<iostream>
