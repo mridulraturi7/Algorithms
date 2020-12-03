9d7212d937208580306011dcf893844d19c7b24e
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-03 20:06:18 +0530

    [Done] DivideandConquerAlgorithm/PoweringNumber/Approach2.cpp

---
+++ Divide and Conquer Algorithm/PoweringNumber/Approach2.cpp
@@ -0,0 +1,33 @@
+/*
+    Powering a Number.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int findPower(int a, int b)
+{
+    if(b == 0)
+    {
+        return 1;
+    }
+
+    return a * findPower(a, b - 1);
+}
+
+int main()
+{
+    int a, b;
+    cout<<"Enter the number(base): ";
+    cin>>a;
+
+    cout<<"Enter the exponent: ";
+    cin>>b;
+
+    int res = findPower(a, b);
+
+    cout<<a<<" to the power "<<b<<" is: "<<res;
+
+    return 0;
+}
