ce54bbab5d2bc2ffcc74934a8c4c6bbb12e1910c
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-03 19:10:22 +0530

    [Done] DivideandConquerAlgorithm/MaximumSubArrayProblem/Implementation.cpp

--- Divide and Conquer Algorithm/MaximumSubArrayProblem/Implementation.cpp
+++ Divide and Conquer Algorithm/MaximumSubArrayProblem/Implementation.cpp
@@ -1,3 +1,76 @@
 /*
     Maximum SubArray Problem.
 */
+
+#include<iostream>
+
+using namespace std;
+
+int max(int x, int y)
+{
+    return x > y ? x : y;
+}
+
+int findCrossSubArraySum(int array[], int l, int mid, int h)
+{
+    int sum = 0;
+    int left_sum = INT_MIN;
+    for(int i = l; i <= mid; i++)
+    {
+        sum += array[i];
+        if(sum > left_sum)
+        {
+            left_sum = sum;
+        }
+    }
+
+    sum = 0;
+    int right_sum = INT_MIN;
+    for(int i = mid + 1; i <= h; i++)
+    {
+        sum += array[i];
+        if(sum > right_sum)
+        {
+            right_sum = sum;
+        }
+    }
+
+    return max(max(left_sum, right_sum), left_sum + right_sum);
+}
+
+int findMaxSubArraySum(int array[], int l, int h)
+{
+    if(l == h)
+    {
+        return array[l];
+    }
+
+    int mid = (l + h) / 2;
+
+    int left_sum = findMaxSubArraySum(array, l, mid);
+
+    int right_sum = findMaxSubArraySum(array, mid + 1, h);
+
+    int cross_sum = findCrossSubArraySum(array, l, mid, h);
+    
+    return max(max(left_sum, right_sum), cross_sum);
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of the Array: ";
+    cin>>n;
+
+    cout<<"Enter Array Elements: ";
+
+    int *array = new int[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin>>array[i];
+    }
+
+    cout<<"Maximum SubArray Sum is: "<<findMaxSubArraySum(array, 0, n - 1);
+
+    return 0;
+}
