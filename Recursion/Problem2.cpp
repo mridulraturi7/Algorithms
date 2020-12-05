290ad06748a90bf0fed66e71c0ce9f5eb22db8fd
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-05 21:04:31 +0530

    [Done] Recursion/Problem2.cpp

--- Recursion/Problem2.cpp
+++ Recursion/Problem2.cpp
@@ -6,6 +6,16 @@
 
 using namespace std;
 
+int numberOfPaths(int m, int n)
+{
+    if(m == 1 || n == 1)
+    {
+        return 1;
+    }
+
+    return numberOfPaths(m, n - 1) + numberOfPaths(m - 1, n);
+}
+
 int main()
 {
     int testCase;
