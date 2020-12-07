64c271036d06ab2ddda5a3b823cf0620cda3dc2b
mridulraturi7 <mridulraturi7@gmail.com>
2020-12-07 22:38:49 +0530

    [Done] Recursion/Problem3.cpp

--- Recursion/Problem3.cpp
+++ Recursion/Problem3.cpp
@@ -6,6 +6,28 @@
 
 using namespace std;
 
+class Solution
+{
+    public:
+        long long toh(int N, int from, int to, int aux)
+        {
+            if(N <= 0)
+            {
+                return 0;
+            }
+
+            static long long moves = 0;
+
+            moves++;
+
+            toh(N - 1, 1, 2, 3);
+            cout<<"move disk "<<N<<" from rod"<<from<<"to"<<to<<endl;
+            toh(N - 1, 1, 3, 2);
+
+            return moves;
+        }
+};
+
 int main()
 {
     int testCase;
@@ -13,7 +35,12 @@ int main()
 
     while(testCase-- != 0)
     {
+        int n;
+        cin>>n;
 
+        Solution ob;
+
+        cout<<ob.toh(n, 1, 3, 2)<<endl;
     }
 
     return 0;
